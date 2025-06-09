3ea29dea97b0c71f1b35a7d0b40f0b7dc59ae26c
prashantvbhise <prashantvbhise@gmail.com>
2025-06-09 08:27:07 +0530

    9th June code

---
+++ src/ex_01_Java_Basic/Lab004_MultiComment.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab004_MultiCommnet {
+    public static void main(String[] args) {
+
+       /* Author:Prashant Bhise
+        * Date Created: 09 June
+        * Purpose: Testing    */
+
+        System.out.println("This is for multiline comment testing");
+
+    }
+}
