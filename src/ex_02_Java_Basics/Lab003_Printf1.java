b977bebbce3ae1e48170844e90993c15751ba3f9
prashantvbhise <prashantvbhise@gmail.com>
2025-06-10 22:05:48 +0530

    10th June commit

--- src/ex_02_Java_Basics/Lab003_Printf1.java
+++ src/ex_02_Java_Basics/Lab003_Printf1.java
@@ -1,4 +1,19 @@
 package ex_02_Java_Basics;
 
 public class Lab003_Printf1 {
+    public static void main(String[] args) {
+        int num=9;
+        int num1=1; int num2=2; int num3=3; int num4=4; int num5=5;
+        int num6=6; int num7=7; int num8=8; int num9=9; int num10=10;
+        System.out.printf("%d * %d =%d\n",num,num1, num*num1  );
+        System.out.printf("%d * %d =%d\n",num,num2, num*num2  );
+        System.out.printf("%d * %d =%d\n",num,num3, num*num3  );
+        System.out.printf("%d * %d =%d\n",num,num4, num*num4  );
+        System.out.printf("%d * %d =%d\n",num,num5, num*num5  );
+        System.out.printf("%d * %d =%d\n",num,num6, num*num6  );
+        System.out.printf("%d * %d =%d\n",num,num7, num*num7  );
+        System.out.printf("%d * %d =%d\n",num,num8, num*num8  );
+        System.out.printf("%d * %d =%d\n",num,num9, num*num9  );
+        System.out.printf("%d * %d =%d\n",num,num10, num*num10  );
+    }
 }
