996db9b196d097b6de8840d35b16ec30cc4c0b31
prashantvbhise <prashantvbhise@gmail.com>
2025-06-19 07:53:03 +0530

    19th June Test

---
+++ src/ex_13_DoWhile/test.java
@@ -0,0 +1,11 @@
+package ex_13_DoWhile;
+
+public class test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(i + j);
+            }
+        }
+    }
+}
---
+++ src/ex_13_Test/Lab00_Challenge1.java
@@ -0,0 +1,15 @@
+package ex_13_Test;
+
+public class Lab00_Challenge1 {
+    public static void main(String[] args) {
+        int age = 10;
+        System.out.println(age);
+        double percentage= 78.76;
+        System.out.println(percentage);
+        boolean eligible= false;
+        System.out.println(eligible);
+        char grade ='A';
+        System.out.println(grade);
+
+    }
+}
