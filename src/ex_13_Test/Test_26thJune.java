5dca71e144e912dd44af6d519b62be0e4d703fff
prashantvbhise <prashantvbhise@gmail.com>
2025-06-26 08:18:06 +0530

    26th June Test

---
+++ src/ex_13_Test/Test_26thJune.java
@@ -0,0 +1,8 @@
+package ex_13_Test;
+
+public class Test_26thJune {
+    public static void main(String[] args) {
+        String  s ="Hello World";
+        System.out.println("Length: "+ s.length()+"," + " First char: " +s.charAt(0)+"," + " Substring: " +s.substring(6,11));
+    }
+}
